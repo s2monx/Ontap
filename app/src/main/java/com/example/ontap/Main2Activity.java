31db892bff43130144f30a7d1e67d2b516bb7542
s2monx <zzprosweet@gmail.com>
2019-09-09 10:40:36 +0700

    commit ontap

--- app/src/main/java/com/example/ontap/Main2Activity.java
+++ app/src/main/java/com/example/ontap/Main2Activity.java
@@ -1,19 +1,24 @@
 package com.example.ontap;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
     EditText etTK, etMK;
+    Button btnThoat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,13 @@ public class Main2Activity extends AppCompatActivity {
         });
         etTK = findViewById(R.id.etTK);
         etMK = findViewById(R.id.etMK);
+        btnThoat = findViewById(R.id.btnThoat);
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
         String tk = getIntent().getExtras().getString("tk");
         String mk = getIntent().getExtras().getString("mk");
         int check = getIntent().getExtras().getInt("chk");
@@ -41,6 +53,27 @@ public class Main2Activity extends AppCompatActivity {
         }
         else
             Toast.makeText(Main2Activity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu", Toast.LENGTH_LONG).show();
+    }
+    public void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thong bao");
+        builder.setMessage("Bạn có muốn đăng xuất không?");
+        builder.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
     }
 
 }
